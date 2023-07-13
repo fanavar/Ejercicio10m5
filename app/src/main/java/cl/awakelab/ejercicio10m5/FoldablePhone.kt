80f246d3be7b29cf511a3db8d4b95f08167e1313
Fabián <fanavar@alumnos.ubiobio.cl>
2023-07-12 21:49:38 -0400

    Initial commit

---
+++ app/src/main/java/cl/awakelab/ejercicio10m5/FoldablePhone.kt
@@ -0,0 +1,16 @@
+package cl.awakelab.ejercicio10m5
+
+class FoldablePhone(isScreenLightOn: Boolean = false) : Phone(isScreenLightOn) {
+    var isFoldable: Boolean = false
+    override fun switchOn() {
+        if (!isFoldable) {
+            isScreenLightOn = true
+        }
+    }
+    fun abrir(){
+        isFoldable = true
+    }
+    fun cerrar(){
+        isFoldable = false
+    }
+}
